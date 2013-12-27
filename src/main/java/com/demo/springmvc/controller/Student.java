23b840f48b9bc6453bb78fe34017b4844439a452
rkridtin <rkridtin@BDC-BKK157>
2013-12-27 09:40:40 +0700

    add Handling Forms

---
+++ src/main/java/com/demo/springmvc/controller/Student.java
@@ -0,0 +1,32 @@
+package com.demo.springmvc.controller;
+
+public class Student {
+	private int age;
+	private String name;
+	private long id;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+}
